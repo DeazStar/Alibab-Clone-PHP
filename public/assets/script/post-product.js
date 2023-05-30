a972ed0a6a6df713afd6e9ef25026940e3b0a5c6
DeazStar <deadfromtheborn@gmail.com>
2023-05-30 22:03:01 +0300

    add post-product.js

---
+++ public/assets/script/post-product.js
@@ -0,0 +1,7 @@
+const listItem = document.querySelectorAll(".list-item");
+const category = document.getElementById("category");
+
+
+listItem.forEach((list) => {
+    list.addEventListener("click", )
+})
